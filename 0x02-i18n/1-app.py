965dc03008a6e34b9a1a98106ba4eda64d1b57c8
oshybobo <oshinnaiye@gmail.com>
2024-07-30 10:11:57 -0700

    babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+1. Basic Babel setup
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+# Configuration for Babel
+
+
+class Config:
+    """Represents a Flask Babel configuration."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """
+    Render the index page.
+
+    Returns:
+        str: The rendered HTML page.
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
